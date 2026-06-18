e18550dde891ecb41757d5b62698742f1542cd80
Sotby <gabrielxp2006@gmail.com>
2026-06-18 16:27:30 -0300

    18-06

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'Inicio', component: Inicio},
+    {path: '', pathMatch: 'full', redirectTo: 'Inicio'},
+    {path: '**', pathMatch: 'full', redirectTo: 'Inicio'}
+];
