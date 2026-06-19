26af0052205bd6bdda7dbc70b55ce83af89f7858
Sotby <gabrielxp2006@gmail.com>
2026-06-19 16:35:32 -0300

    19-06

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './paginas/inicio/inicio';
+import { Cadastro } from './paginas/cadastro/cadastro';
+import { Login } from './paginas/login/login';
 
 export const routes: Routes = [
     {path: 'Inicio', component: Inicio},
-    {path: '', pathMatch: 'full', redirectTo: 'Inicio'},
+    {path: 'Cadastro', component: Cadastro},
+    {path: 'Login', component: Login},
     {path: '**', pathMatch: 'full', redirectTo: 'Inicio'}
 ];
